8eba43e83ef349046e29f71837a941893b0b3122
ginkoc <chenchm@phy.com>
2020-01-14 10:41:26 +0800

    完善切面获得注解的逻辑

--- checker/src/main/java/com/ginko/license/checker/weaver/CheckPointWeaver.java
+++ checker/src/main/java/com/ginko/license/checker/weaver/CheckPointWeaver.java
@@ -3,13 +3,14 @@ package com.ginko.license.checker.weaver;
 import com.ginko.license.checker.annotations.CheckPoint;
 import com.ginko.license.checker.core.Checker;
 import com.ginko.license.checker.exception.CheckException;
-import com.ginko.license.checker.predicates.AbstractLicensePredicate;
 import com.ginko.license.checker.predicates.LicensePredicate;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
@@ -21,6 +22,8 @@ import java.lang.reflect.Method;
  */
 @Aspect
 public class CheckPointWeaver {
+
+    private static final Logger log = LoggerFactory.getLogger(CheckPointWeaver.class);
 
     @Pointcut("execution(* *(..)) && @annotation(com.ginko.license.checker.annotations.CheckPoint)")
     public void jointPoint() {
@@ -41,10 +44,13 @@ public class CheckPointWeaver {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Method method = signature.getMethod();
 
-        if (method.isAnnotationPresent(annotation)) {
-            return method.getAnnotation(annotation);
+        try {
+            // 防止方法通过动态代理或者cglib代理后无法获得真实的注解
+            Method realMethod = joinPoint.getTarget().getClass().getDeclaredMethod(signature.getName(), method.getParameterTypes());
+            return realMethod.isAnnotationPresent(annotation) ? realMethod.getAnnotation(annotation) : null;
+        } catch (NoSuchMethodException e) {
+            log.error("Get method" + method + "annotations failed!", e);
+            throw new RuntimeException("Get method" + method + "annotations failed!");
         }
-
-        return null;
     }
 }
